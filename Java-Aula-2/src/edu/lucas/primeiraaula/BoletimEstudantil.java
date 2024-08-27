1557d274c552ae50eb731cbca9400d4e11617e0b
Lucas Oliveira <lucasantonio97@live.com>
2024-08-27 17:40:55 -0300

    Introdução aos packages

--- Java-Aula-2/src/edu/lucas/primeiraaula/BoletimEstudantil.java
+++ Java-Aula-2/src/edu/lucas/primeiraaula/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.lucas.primeiraaula;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 5;
--- Java-Aula-2/src/edu/lucas/segundaaula/MinhaClasse.java
+++ Java-Aula-2/src/edu/lucas/segundaaula/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.lucas.segundaaula;
 public class MinhaClasse {
 
     public static void main(String[] args) {
