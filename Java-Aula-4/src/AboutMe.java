eb13d38da46f0b5cde9eee938ddb51f778b10f82
Lucas Oliveira <lucasantonio97@live.com>
2024-08-29 16:59:20 -0300

    Tratamento de exceções

--- Java-Aula-4/src/AboutMe.java
+++ Java-Aula-4/src/AboutMe.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class AboutMe {
     public static void main(String[] args)  {
+        try {
        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
     
        System.out.println("Digite seu nome: ");
@@ -11,9 +13,19 @@ public class AboutMe {
        System.out.println("Digite sua idade: ");
        int idade = scanner.nextInt();
 
-       System.out.println("Seu nome é " + nome + " e sua idade é " + idade);
+       System.out.println("Digite sua altura: ");
+       double altura = scanner.nextDouble();
+
+       System.out.println("Digite seu peso: ");
+       double peso = scanner.nextDouble();
+
+       System.out.println("Seu nome é " + nome + " e sua idade é " + idade + " sua altura é " + altura + " e seu peso é " + peso);
 
        
         scanner.close();
+        }catch (InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser númericos");
+        }
+
     }
 }
