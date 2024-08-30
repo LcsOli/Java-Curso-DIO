ab92bd443625308c964c64ae64ef06f2927b98f6
Lucas Oliveira <lucasantonio97@live.com>
2024-08-30 14:51:44 -0300

    Desafio Contador

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+
+public class ParametrosInvalidosException extends RuntimeException {
+        public ParametrosInvalidosException() {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+}
