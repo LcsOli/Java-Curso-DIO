3dbfde9508e1c977ad7446126b9c973560c5fc52
Lucas Oliveira <lucasantonio97@live.com>
2024-08-30 14:52:22 -0300

    Atualizando codigo

--- Java-Aula-4/src/AboutMe.java
+++ Java-Aula-4/src/AboutMe.java
@@ -3,27 +3,27 @@ import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class AboutMe {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
-       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-    
-       System.out.println("Digite seu nome: ");
-       String nome = scanner.next();
-    
-       System.out.println("Digite sua idade: ");
-       int idade = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-       System.out.println("Digite sua altura: ");
-       double altura = scanner.nextDouble();
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
 
-       System.out.println("Digite seu peso: ");
-       double peso = scanner.nextDouble();
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
 
-       System.out.println("Seu nome é " + nome + " e sua idade é " + idade + " sua altura é " + altura + " e seu peso é " + peso);
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-       
-        scanner.close();
-        }catch (InputMismatchException e){
+            System.out.println("Digite seu peso: ");
+            double peso = scanner.nextDouble();
+
+            System.out.println("Seu nome é " + nome + " e sua idade é " + idade + " sua altura é " + altura
+                    + " e seu peso é " + peso + " KG");
+
+            scanner.close();
+        } catch (InputMismatchException e) {
             System.out.println("Os campos idade e altura precisam ser númericos");
         }
 
